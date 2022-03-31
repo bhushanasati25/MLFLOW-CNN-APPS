b0c713c9f8b762a76bb8ac05173de91b2c6d43af
Bhushan <bhushanasati25@gmail.com>
2022-03-31 11:33:48 +0530

    Cod

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "MLFLow-Project-Template"
-AUTHOR_USER_NAME = "bhushanasati25"
+REPO_NAME = "MLflow-project-template"
+AUTHOR_USER_NAME = "c17hawke"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -18,7 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="bhushanasati25@gmail.com",
+    author_email="sunny.c17hawke@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
